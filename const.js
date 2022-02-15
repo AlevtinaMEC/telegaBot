2cffdd8b38b38817a902ae54c8de9af0d9bb2e5e
Alevtina Matiushenko <alevtina.matiushenko@simpleone.ru>
2022-02-15 09:19:58 +0300

    first commit

---
+++ const.js
@@ -0,0 +1,21 @@
+const commands = `
+/start - Перезапустить бота
+/help - Помощь
+`;
+
+const text1 = `
+1.Текст для првоерки обработчика и <a href="https://youtube.com/">Ссылка</a>
+`;
+
+const text2 = `
+2/Текст для првоерки обработчика и <a href="https://youtube.com/">Ссылка</a>
+`;
+
+const text3= `
+3.Текст для првоерки обработчика и <a href="https://youtube.com/">Ссылка</a>
+`;
+
+module.exports.commands = commands;
+module.exports.text1 = text1;
+module.exports.text2 = text2;
+module.exports.text3 = text3;
